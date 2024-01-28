635cfcdb232d757004b08e8062f6ca90720a8918
DINAMOW <meemoo102039@gmail.com>
2024-01-28 10:48:48 +0200

    making start and end var annonymet_typed

--- 0-simple_helper_function.py
+++ 0-simple_helper_function.py
@@ -13,6 +13,6 @@ def index_range(page: int, page_size: int) -> tuple:
         Returns:
             tuple: (start_index, end_index)
     """
-    start_index = (page - 1) * page_size
-    end_index = page * page_size
+    start_index: int = (page - 1) * page_size
+    end_index: int = page * page_size
     return (start_index, end_index)
